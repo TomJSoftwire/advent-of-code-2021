efa13e615a226f6c733a362e23760c3d0dae3c45
TomJSoftwire <74607461+TomJSoftwire@users.noreply.github.com>
2021-12-01 11:50:48 +0000

    completed day 1

--- 01/sol1.py
+++ 01/sol1.py
@@ -1,3 +1,11 @@
 with open('input.txt') as data:
     input = data.read().split('\n')[:-1]
 
+count = 0
+previous = int(input[0])
+for reading in input:
+    if int(reading) > previous:
+        count += 1
+    previous = int(reading)
+
+print(count)
--- 01/sol2.py
+++ 01/sol2.py
@@ -1,3 +1,15 @@
 with open('input.txt') as data:
     input = data.read().split('\n')[:-1]
 
+inputInt = list(map(lambda n: int(n), input))
+count2 = 0
+index = 0
+previous = inputInt[index] + inputInt[index + 1] + inputInt[index + 2]
+while index < len(input) - 2:
+    group = inputInt[index] + inputInt[index + 1] + inputInt[index + 2]
+    if group > previous:
+        count2+=1
+    previous = group
+    index += 1
+
+print(count2)
