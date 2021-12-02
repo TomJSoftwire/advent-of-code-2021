2d49ecdbca4ce5b5134654889a6a1bca0d72d9a4
TomJSoftwire <74607461+TomJSoftwire@users.noreply.github.com>
2021-12-02 09:50:52 +0000

    completed day 2

--- 02/sol1.py
+++ 02/sol1.py
@@ -1,3 +1,18 @@
 with open('input.txt') as data:
     input = data.read().split('\n')[:-1]
 
+x = 0
+y = 0
+
+for i in input:
+    inst = str.split(i, ' ')
+    dir = inst[0]
+    mag = int(inst[1])
+    if dir == 'forward':
+        x += mag
+    if dir == 'down':
+        y += mag
+    if dir == 'up':
+        y -= mag
+
+print(x * y)
--- 02/sol2.py
+++ 02/sol2.py
@@ -1,3 +1,23 @@
 with open('input.txt') as data:
     input = data.read().split('\n')[:-1]
 
+with open('input.txt') as data:
+    input = data.read().split('\n')[:-1]
+
+x = 0
+y = 0
+a = 0
+
+for i in input:
+    inst = str.split(i, ' ')
+    dir = inst[0]
+    mag = int(inst[1])
+    if dir == 'forward':
+        x += mag
+        y += mag * a
+    if dir == 'down':
+        a += mag
+    if dir == 'up':
+        a -= mag
+
+print(x * y)
