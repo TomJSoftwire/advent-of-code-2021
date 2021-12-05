9f0af864deb0b8c92b7c5f30ec4410ae17131112
TomJSoftwire <74607461+TomJSoftwire@users.noreply.github.com>
2021-12-05 14:32:00 +0000

    completed day 3

--- 03/sol1.py
+++ 03/sol1.py
@@ -1,3 +1,22 @@
+import statistics
+
 with open('input.txt') as data:
     input = data.read().split('\n')[:-1]
 
+most = ''
+least = ''
+
+len = len(input[0])
+for i in range(0, len):
+    bits = list(map(lambda x: int(x[i]), input))
+    avgBit = statistics.mean(bits)
+    if(avgBit > 0.5):
+        most += '1'
+        least += '0'
+    else:
+        most += '0'
+        least += '1'
+numA = int(most,2)
+numB = int(least,2)
+
+print(numB * numA)
--- 03/sol2.py
+++ 03/sol2.py
@@ -1,3 +1,31 @@
+import statistics
+
 with open('input.txt') as data:
     input = data.read().split('\n')[:-1]
 
+most = list(input)
+least = list(input)
+
+binNumLen = len(input[0])
+cur = 0
+while (len(most) > 1):
+    bits = list(map(lambda x: int(x[cur]), most))
+    avgBit = statistics.mean(bits)
+    most = list(filter(lambda x: x[cur] == '1' if avgBit >= 0.5 else x[cur] == '0', most))
+    cur += 1
+    print(most)
+
+cur = 0
+while (len(least) > 1):
+    bits = list(map(lambda x: int(x[cur]), least))
+    avgBit = statistics.mean(bits)
+    least = list(filter(lambda x: x[cur] == '1' if avgBit < 0.5 else x[cur] == '0', least))
+    cur += 1
+    print(least)
+
+numA = int(most[0],2)
+numB = int(least[0],2)
+
+print(numA)
+
+print(numB * numA)
