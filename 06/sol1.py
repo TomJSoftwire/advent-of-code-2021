5dcf03b96521e55c540373164dd2822ca11a7704
TomJSoftwire <74607461+TomJSoftwire@users.noreply.github.com>
2021-12-06 16:56:40 +0000

    completed day 6

--- 06/sol1.py
+++ 06/sol1.py
@@ -1,3 +1,18 @@
 with open('input.txt') as data:
-    input = data.read().split('\n')[:-1]
+    input = list(map(lambda num: int(num), data.read().split(',')))
 
+print(len(input))
+print(input)
+
+for day in range(0,80):
+    print(f'day {day}')
+    for i in range(0, len(input)):
+        fish = input[i]
+        if fish == 0:
+            input.append(8)
+            input[i] = 6
+        else:
+            input[i] -= 1
+
+print(len(input))
+    
--- 06/sol2.py
+++ 06/sol2.py
@@ -1,3 +1,23 @@
 with open('input.txt') as data:
-    input = data.read().split('\n')[:-1]
+    input = list(map(lambda num: int(num), data.read().split(',')))
 
+print(len(input))
+# print(input)
+
+counts = [0,0,0,0,0,0,0,0,0]
+for fish in input:
+    counts[fish] += 1
+
+for day in range(0,256):
+    newFish = counts[0]
+    counts = counts[1:7] + [counts[0] + counts[7]] + [counts[8]] + [counts[0]]
+    print(counts)
+
+total = 0
+
+for c in counts:
+    total += c
+print(counts)
+print(total)
+    
+  
