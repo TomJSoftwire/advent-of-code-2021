2d80869be7d95387bf51bd4aa0f25232f03944d9
TomJSoftwire <74607461+TomJSoftwire@users.noreply.github.com>
2021-12-07 10:09:28 +0000

    completed day 7

--- 07/sol1.py
+++ 07/sol1.py
@@ -1,3 +1,15 @@
 with open('input.txt') as data:
-    input = data.read().split('\n')[:-1]
+    inputCrab = list(map(lambda x: int(x), data.read().split(',')))
 
+def findMinFuel(input):
+    minFuel = 9999999999999
+    for pos in range(0,max(input) + 1):
+        fuel = 0 
+        for crab in input:
+            fuel += abs(crab - pos)
+        if fuel < minFuel:
+            minFuel = fuel
+
+    print(minFuel)         
+
+findMinFuel(inputCrab)
--- 07/sol2.py
+++ 07/sol2.py
@@ -1,3 +1,16 @@
 with open('input.txt') as data:
-    input = data.read().split('\n')[:-1]
+    inputCrab = list(map(lambda x: int(x), data.read().split(',')))
 
+def findMinFuel(input):
+    minFuel = 9999999999999
+    for pos in range(0,max(input) + 1):
+        fuel = 0 
+        for crab in input:
+            dist = abs(crab - pos)
+            fuel += int(0.5 * dist * (dist + 1))
+        if fuel < minFuel:
+            minFuel = fuel
+
+    print(minFuel)         
+
+findMinFuel(inputCrab)
