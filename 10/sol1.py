49d23eaca5f7632dff5378d5285ccb100478ca22
TomJSoftwire <74607461+TomJSoftwire@users.noreply.github.com>
2021-12-11 14:17:03 +0000

    completed day 10

--- 10/sol1.py
+++ 10/sol1.py
@@ -1,3 +1,34 @@
 with open('input.txt') as data:
     input = data.read().split('\n')[:-1]
 
+# print(input)
+
+def is_corrupt(line):
+    open = ['(', '{' ,'[', '<']
+    close = [')', '}' ,']', '>']
+    nextClose = [None]
+    for char in list(line):
+
+        if char in close:
+            if char == nextClose[0]:
+                nextClose.pop(0)
+            else:
+                return True, char
+        elif char in open:
+            ind = open.index(char)
+            nextClose.insert(0,close[ind])
+    
+    return False, ''
+points = {
+    ')': 3,
+    ']': 57,
+    '}': 1197,
+    '>': 25137
+}
+count = 0
+for line in input:
+    is_broken, breaking_char = is_corrupt(line)
+    if is_broken:
+        count += points[breaking_char]
+
+print(count)
--- 10/sol2.py
+++ 10/sol2.py
@@ -1,3 +1,51 @@
+import statistics
 with open('input.txt') as data:
     input = data.read().split('\n')[:-1]
 
+def is_corrupt(line):
+    open = ['(', '{' ,'[', '<']
+    close = [')', '}' ,']', '>']
+    nextClose = [None]
+    for char in list(line):
+        if char in close:
+            if char == nextClose[0]:
+                nextClose.pop(0)
+            else:
+                return True, char
+        elif char in open:
+            ind = open.index(char)
+            nextClose.insert(0,close[ind])
+    
+    return False, ''
+points = {
+    ')': 1,
+    ']': 2,
+    '}': 3,
+    '>': 4
+}
+
+def get_closing_chars(string):
+    open = ['(', '{' ,'[', '<']
+    close = [')', '}' ,']', '>']
+    nextClose = []
+    for char in list(line):
+        if char in close:
+            nextClose.pop(0)
+        elif char in open:
+            ind = open.index(char)
+            nextClose.insert(0,close[ind])
+    return nextClose
+
+linePoints = []
+for line in input:
+    is_broken, breaking_char = is_corrupt(line)
+    if not is_broken:
+        print(get_closing_chars(line))
+        score = 0
+        for char in get_closing_chars(line):
+            score *= 5
+            score += points[char]
+        linePoints.append(score)
+        
+
+print(statistics.median(linePoints))
