bd6b431e16aaa1d83b95ba2edc31042f7d9b7027
TomJSoftwire <74607461+TomJSoftwire@users.noreply.github.com>
2021-12-18 22:05:46 +0000

    completed day 17

--- 17/sol1.py
+++ 17/sol1.py
@@ -1,3 +1,52 @@
 with open('input.txt') as data:
     input = data.read().split('\n')[:-1]
+xTarget = [281,311]
+yTarget = [-74, -54]
+
+def getNextPos(pos, vx, vy):
+    posN = list(pos)
+    posN[0] += vx
+    posN[1] += vy
+
+    vyN = vy - 1
+    vxN = vx - (1 if vx > 0 else 0)
+
+    return (posN, vxN, vyN)
+
+
+def getStepsForV(v):
+    vx = v[0]
+    vy = v[1]
+    pos = [0,0]
+    steps = []
+    while pos[0] <= xTarget[1] and pos[1] >= yTarget[0]:
+        steps.append(pos)
+        pos, vx, vy = getNextPos(pos, vx, vy)
+    return steps
+
+def vHitsTarget(v):
+    lastStep = getStepsForV(v)[-1]
+    x = lastStep[0]
+    y = lastStep[1]
+    return x >= xTarget[0] and x <= xTarget[1] and y >= yTarget[0] and y <= yTarget[1]
+
+def vyCanHit(vy):
+    vx = 0
+    while vx <= xTarget[1]:
+        if(vHitsTarget([vx,vy])):
+            return True, [vx, vy]
+        vx += 1
+    return False
+
+vy = 0
+mvy = 0
+while(vy < abs(yTarget[0])):
+    vy += 1
+    if(vyCanHit(vy)):
+        mvy = vy
+
+def get_apex(vy):
+    return int(0.5 * vy * (vy + 1))
+print(mvy)
+print(get_apex(mvy))        
 
--- 17/sol2.py
+++ 17/sol2.py
@@ -1,3 +1,38 @@
 with open('input.txt') as data:
     input = data.read().split('\n')[:-1]
+xTarget = [281,311]
+yTarget = [-74, -54]
 
+def getNextPos(pos, vx, vy):
+    posN = list(pos)
+    posN[0] += vx
+    posN[1] += vy
+
+    vyN = vy - 1
+    vxN = vx - (1 if vx > 0 else 0)
+
+    return (posN, vxN, vyN)
+
+
+def getStepsForV(v):
+    vx = v[0]
+    vy = v[1]
+    pos = [0,0]
+    steps = []
+    while pos[0] <= xTarget[1] and pos[1] >= yTarget[0]:
+        steps.append(pos)
+        pos, vx, vy = getNextPos(pos, vx, vy)
+    return steps
+
+def vHitsTarget(v):
+    lastStep = getStepsForV(v)[-1]
+    x = lastStep[0]
+    y = lastStep[1]
+    return x >= xTarget[0] and x <= xTarget[1] and y >= yTarget[0] and y <= yTarget[1]
+
+count = 0 
+for vy in range(-abs(yTarget[0]), abs(yTarget[0])):
+      for vx in range(0, xTarget[1] + 1):
+          if vHitsTarget([vx,vy]):
+              count += 1
+print(count)
